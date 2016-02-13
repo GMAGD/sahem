93dd8d0cad5187ca04f203ec80e53ba827972583
ilyes hammadi <hammadiilyesahmed@gmail.com>
2016-02-13 20:48:57 +0100

    add events application

--- config/settings/common.py
+++ config/settings/common.py
@@ -45,6 +45,7 @@ THIRD_PARTY_APPS = (
 LOCAL_APPS = (
     'sahem.users',  # custom users app
     # Your stuff: custom apps go here
+    'sahem.events',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
-
+    url(r'^events/', include('sahem.events.urls', namespace='events'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
