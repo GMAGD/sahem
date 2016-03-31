f06cd6ad571a2534a5eb6daaf8857cf69da7dc5a
fcmam5 <dr.fcmam5@gmail.com>
2016-03-31 09:56:15 +0100

    Setup a navbar

--- sahem/static/js/project.js
+++ sahem/static/js/project.js
@@ -74,9 +74,7 @@ function createEvent() {
 }
 
 $(document).ready(function () {
-    $('.collapsible').collapsible({
-        accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
+    $(".button-collapse").sideNav();
 });
 
 $(document).ready(function () {
